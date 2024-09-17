d170298a5bd0609568220ca5b812a448b0cc8a85
saef mohamed <143071542+Dev-SaefMohamed@users.noreply.github.com>
2024-09-17 22:21:28 +0300

    Product feature

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./routes/userRoute"
+import productRoute from "./routes/productRoute"
+import { seedInitialProduct } from "./services/productService";
 
 const app = express();
 const port = 3001;
@@ -11,7 +13,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/ecommerce')
   .then(() => console.log('Mongo Connected!'))
   .catch((err) => console.log('Failed to connect!', err));
 
+// seed the products to the DB
+seedInitialProduct()
+
 app.use('/user', userRoute)
+
+app.use('/products', productRoute)
 
 app.listen(port, () => {
     console.log(`Server is running at: http://localhost:${port}`);
---
+++ src/services/productService.ts
@@ -0,0 +1,18 @@
+import productModel from "../models/productModel";
+
+export const getAllProducts = async () => {
+       return await productModel.find();
+}
+
+export const seedInitialProduct = async () => {
+    const products = [
+        { title: "Dell Laptop", image: "https://i5.walmartimages.com/seo/Dell-Inspiron-15-6-FHD-Touchscreen-Laptop-Computer-AMD-Ryzen-5-8GB-DDR4-RAM-256GB-SSD-HDMI-USB-3-1-Windows-10-Home_c761828d-f59d-4d87-a380-33740e087477.12423ff52be2c3671d251794ff0afca0.jpeg", price: 25000, stock: 13 }
+    ];
+
+    const existingProducts = await getAllProducts();
+
+    if(existingProducts.length === 0){
+        await productModel.insertMany(products)
+    }
+
+};
